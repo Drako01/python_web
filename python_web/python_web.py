1eb49dd6cd6dc9be7153b14bc6998738660f2dc0
Alejandro Di Stefano <addistefano@live.com.ar>
2024-10-06 19:30:51 -0300

    first commit

---
+++ python_web/python_web.py
@@ -0,0 +1,52 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+
+from rxconfig import config
+
+
+class State(rx.State):
+    """The app state."""
+
+    ...
+
+
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to Reflex! Alejandro", size="9"),
+            rx.text("Hola Alejandro", color="red", font_size="1.5rem"),
+            rx.text(
+                "Get started by editing ",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                size="5",
+            ),
+            rx.link(
+                rx.button("Check out our docs!"),
+                href="https://reflex.dev/docs/getting-started/introduction/",
+                is_external=True,
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+        rx.button(
+            "Fancy Button",
+            border_radius="1em",
+            box_shadow="rgba(151, 65, 252, 0.8) 0 15px 30px -10px",
+            background_image="linear-gradient(144deg,#AF40FF,#5B42F3 50%,#00DDEB)",
+            box_sizing="border-box",
+            color="white",
+            opacity=1,
+            _hover={
+                "opacity": 0.5,
+            },
+        ),
+        rx.logo(),
+    )
+
+
+app = rx.App()
+app.add_page(index)
